40ce5eadb259a708dc394dca4eb05761ba932b40
cemill26 <cemill26@asu.edu>
2024-10-18 21:56:44 -0400

    Connect settings page to icon and make it a modal

--- satellite_experience/assets/js/main.js
+++ satellite_experience/assets/js/main.js
@@ -4,14 +4,18 @@ document.addEventListener("DOMContentLoaded", function() {
     var helpModal = document.getElementById("help-modal");
     var helpModalContent = document.getElementById("help-modal-content");
 
-    // Inject the help page modal content
+    // Settings modal elements
+    var settingsModal = document.getElementById("settings-modal");
+    var settingsModalContent = document.getElementById("settings-modal-content");
+
+    // Inject the help modal content
     function openHelpModal() {
         var xhr = new XMLHttpRequest();
         xhr.open("GET", "help_page.html", true);
         xhr.onreadystatechange = function() {
             if (xhr.readyState == 4 && xhr.status == 200) {
                 helpModalContent.innerHTML = xhr.responseText;
-                helpModal.style.display = "flex"; // Show modal when content is loaded
+                helpModal.style.display = "flex";
             }
         };
         xhr.send();
@@ -23,13 +27,13 @@ document.addEventListener("DOMContentLoaded", function() {
         openHelpModal();
     });
 
-    // Close the help page modal
-    var modalCloseButton = document.querySelector(".modal-popup .modal-close");
-    modalCloseButton.onclick = function() {
+    // Close the help modal
+    var helpModalCloseButton = document.getElementById("help-modal-close");
+    helpModalCloseButton.addEventListener("click", function() {
         helpModal.style.display = "none";
-    };
+    });
 
-    // Close the help page modal if clicking out
+    // Close the help modal if clicking out
     window.onclick = function(event) {
         if (event.target == helpModal) {
             helpModal.style.display = "none";
@@ -44,7 +48,10 @@ document.addEventListener("DOMContentLoaded", function() {
     function resetInactivityTimer() {
         clearTimeout(inactivityTimer);
         inactivityTimer = setTimeout(function() {
-            openHelpModal();
+            // Check that settings page isn't open
+            if (settingsModal.style.display !== "flex") {
+                openHelpModal();
+            }
         }, inactivityTime)
     }
 
@@ -57,4 +64,37 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Initialize inactivity timer
     resetInactivityTimer();
+
+    // Inject the settings modal content
+    function openSettingsModal() {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "settings_page.html", true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                settingsModalContent.innerHTML = xhr.responseText;
+                settingsModal.style.display = "flex";
+            }
+        };
+        xhr.send();
+    }
+
+    // Settings icon button
+    var settingsIconButton = document.getElementById("settings-icon-button");
+    settingsIconButton.addEventListener("click", function() {
+        openSettingsModal();
+    });
+
+    // Close the settings modal
+    var settingsModalCloseButton = document.getElementById("settings-modal-close");
+    settingsModalCloseButton.addEventListener("click", function() {
+        resetInactivityTimer(); // Reset activity timer so it doesn't pop up as soon as settings is closed
+        settingsModal.style.display = "none";
+    });
+
+    // Close the settings modal if clicking out
+    window.onclick = function(event) {
+        if (event.target == settingsModal) {
+            settingsModal.style.display = "none";
+        }
+    };
 });
