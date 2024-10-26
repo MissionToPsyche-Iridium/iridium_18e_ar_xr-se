3bbba3476f7b13fda4c06e2915cef92177cfe445
nswangarcia <nswanso6@asu.edu>
2024-10-26 14:02:57 -0500

    US75Task139: add main page elements to skybox

--- satellite_experience/assets/js/skybox.js
+++ satellite_experience/assets/js/skybox.js
@@ -158,4 +158,105 @@ window.addEventListener('DOMContentLoaded', () => {
   _APP = new SpaceSkybox();
 });
 
+// Ensure DOM content has loaded
+document.addEventListener("DOMContentLoaded", function() {
+    // Help modal elements
+    var helpModal = document.getElementById("help-modal");
+    var helpModalContent = document.getElementById("help-modal-content");
+
+    // Settings modal elements
+    var settingsModal = document.getElementById("settings-modal");
+    var settingsModalContent = document.getElementById("settings-modal-content");
+
+    // Inject the help modal content
+    function openHelpModal() {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "help_page.html", true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                helpModalContent.innerHTML = xhr.responseText;
+                helpModal.style.display = "flex";
+            }
+        };
+        xhr.send();
+    }
+
+    // Help icon button
+    var helpIconButton = document.getElementById("help-icon-button");
+    helpIconButton.addEventListener("click", function() {
+        openHelpModal();
+    });
+
+    // Close the help modal
+    var helpModalCloseButton = document.getElementById("help-modal-close");
+    helpModalCloseButton.addEventListener("click", function() {
+        helpModal.style.display = "none";
+    });
+
+    // Close the help modal if clicking out
+    window.onclick = function(event) {
+        if (event.target == helpModal) {
+            helpModal.style.display = "none";
+        }
+    };
+
+    // Track user activity
+    var inactivityTime = 60000; // 60 second timer
+    var inactivityTimer;
+
+    // Open help modal if timer reaches inactivity time
+    function resetInactivityTimer() {
+        clearTimeout(inactivityTimer);
+        inactivityTimer = setTimeout(function() {
+            // Check that settings page isn't open
+            if (settingsModal.style.display !== "flex") {
+                openHelpModal();
+            }
+        }, inactivityTime)
+    }
+
+    // Events to reset the inactivity time i.e. user interaction
+    window.addEventListener('mousemove', resetInactivityTimer);
+    window.addEventListener('keydown', resetInactivityTimer);
+    window.addEventListener('click', resetInactivityTimer);
+    window.addEventListener('touchstart', resetInactivityTimer);
+    window.addEventListener('scroll', resetInactivityTimer);
+
+    // Initialize inactivity timer
+    resetInactivityTimer();
+
+    // Inject the settings modal content
+    function openSettingsModal() {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "settings_page.html", true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                settingsModalContent.innerHTML = xhr.responseText;
+                settingsModal.style.display = "flex";
+            }
+        };
+        xhr.send();
+    }
+
+    // Settings icon button
+    var settingsIconButton = document.getElementById("settings-icon-button");
+    settingsIconButton.addEventListener("click", function() {
+        openSettingsModal();
+    });
+
+    // Close the settings modal
+    var settingsModalCloseButton = document.getElementById("settings-modal-close");
+    settingsModalCloseButton.addEventListener("click", function() {
+        resetInactivityTimer(); // Reset activity timer so it doesn't pop up as soon as settings is closed
+        settingsModal.style.display = "none";
+    });
+
+    // Close the settings modal if clicking out
+    window.onclick = function(event) {
+        if (event.target == settingsModal) {
+            settingsModal.style.display = "none";
+        }
+    };
+});
+
 
