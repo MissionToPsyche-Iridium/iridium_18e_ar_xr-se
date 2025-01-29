8f4f339801ce217d257accdf552f17b2998d5899
nswangarcia <nswanso6@asu.edu>
2025-01-29 14:14:51 -0600

    US263Task285: update papyrus scroll to banner, update intro text. Hide main text until intro popup closes. Add transition between both.

--- lore_and_mythology_experience/assets/js/intro.js
+++ lore_and_mythology_experience/assets/js/intro.js
@@ -1,14 +1,22 @@
 export function openPopup() {
     const popup = document.getElementById("papyrus-scroll-popup");
     if (popup) {
-        popup.style.display = "block";
-        setTimeout(closePopup, 20000); // Close after 20 seconds
+        popup.style.visibility = "visible";
+        popup.style.opacity = "1";
+        setTimeout(closePopup, 5000); // Close after 5 seconds
     }
 }
 
 export function closePopup() {
     const popup = document.getElementById("papyrus-scroll-popup");
     if (popup) {
-        popup.style.display = "none";
+        popup.style.visibility = "hidden";
+        popup.style.opacity = "0";
+    }
+
+    const mainTitle = document.querySelector(".main-title");
+    if (mainTitle) {
+        mainTitle.style.visibility = "visible";
+        mainTitle.style.opacity = "1";
     }
 }
