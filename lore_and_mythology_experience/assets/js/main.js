62c4e53df2b7e62ee9e7323ae7a117f458e270bc
nswangarcia <nswanso6@asu.edu>
2025-02-07 23:55:34 -0600

    US284Task298 and Task292: implement goddess phase and phase transitions

--- lore_and_mythology_experience/assets/js/main.js
+++ lore_and_mythology_experience/assets/js/main.js
@@ -1,5 +1,6 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js";
 import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js";
+import { startPhases } from "./phases.js";
 // import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/loaders/GLTFLoader.js";
 // import {
 //     CSS2DRenderer,
@@ -164,8 +165,6 @@ function moveScope(event) {
         const startPos = camera.position.clone();
         const startTarget = controls.target.clone();
 
-        showAnnibale();
-
         function animateZoom(time) {
             const elapsed = time - startTime;
             const t = Math.min(elapsed / duration, 1);
@@ -183,50 +182,13 @@ function moveScope(event) {
         }
         requestAnimationFrame(animateZoom);
     } else {
-        document.getElementById("annibale_modal").setAttribute("style", "display: 'none';");
-        document.getElementById("annibale").setAttribute("style", "display: 'none';");
-        document.getElementById("papyrus_double_sided").setAttribute("style", "display: 'none';");
-        document.getElementById("scroll_text_box").setAttribute("style", "display: 'none';");
-    }
+            document.getElementById("phase_modal").setAttribute("style", "display: 'none';");
+            document.getElementById("phase").setAttribute("style", "display: 'none';");
+            document.getElementById("papyrus_scroll").setAttribute("style", "display: 'none';");
+            document.getElementById("scroll_text_box").setAttribute("style", "display: 'none';");
+        }
     count = 100;
 }
-
-let annibool = false;
-function showAnnibale() {
-    if (!annibool) {
-        const annibale_div = document.createElement("div");
-        annibale_div.setAttribute("id", "annibale_modal");
-        annibale_div.setAttribute("style", "display: 'block'; position: fixed; z-index: 20; left: 0; top: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 1);");
-
-        let annibale_innerHTML = "";
-        annibale_innerHTML += `<img src="../assets/images/annibale.jpg" id="annibale"/>
-                               <img src="../assets/images/papyrus_scroll_double_sided.png" id="papyrus_double_sided"/>
-                               <div id="scroll_text_box">
-                                 <span class="info">On March 17, 1852, the Italian astronomer</span>
-                                 <span class="info">Annibale de Gasparis discovered the 16th</span>
-                                 <span class="info">asteroid in the main asteroid belt</span>
-                                 <span class="info">between Mars and Jupiter.</span>
-                                 <span class="info">De Gasparis named this asteroid Psyche.</span>
-                               </div>`;
-        annibale_div.innerHTML = annibale_innerHTML;
-        document.body.appendChild(annibale_div);
-
-        document.getElementById("annibale").setAttribute("style", "background-color: transparent; width: calc(0.8 * 40vh); height: 40vh; border-radius: 12px; padding: 5vh; position: absolute; top: calc(0.25 * 40vh); left: calc(50vw - ((0.8 * 40vh + 10vh) / 2));");
-        document.getElementById("papyrus_double_sided").setAttribute("style", "background-color: transparent; width: 40vh; height: 40vh; border-radius: 12px; position: absolute; top: 50vh; left: calc(50vw - (40vh / 2));");
-        document.getElementById("scroll_text_box").setAttribute("style", "display: flex; flex-direction: column; position: absolute; width: 40vh; height: calc(40vh / 2); top: calc(50vh + ((40vh / 1.69) / 1.69)); left: calc(50vw - (40vh / 2));");
-        var infos = document.getElementsByClassName("info");
-        for (var i = 0; i < infos.length; i++) {
-            infos[i].setAttribute("style", "text-align: center; font-size: calc(0.045 * 40vh); z-index: 21;");
-        }
-        annibool = true;
-    } else {
-        document.getElementById("annibale_modal").setAttribute("style", "display: 'block'; position: fixed; z-index: 20; left: 0; top: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 1);");
-        document.getElementById("annibale").setAttribute("style", "background-color: transparent; width: calc(0.8 * 40vh); height: 40vh; border-radius: 12px; padding: 5vh; position: absolute; top: calc(0.25 * 40vh); left: calc(50vw - ((0.8 * 40vh + 10vh) / 2));");
-        document.getElementById("papyrus_double_sided").setAttribute("style", "background-color: transparent; width: 40vh; height: 40vh; border-radius: 12px; position: absolute; top: 50vh; left: calc(50vw - (40vh / 2));");
-        document.getElementById("scroll_text_box").setAttribute("style", "display: flex; flex-direction: column; position: absolute; width: 40vh; height: calc(40vh / 2); top: calc(50vh + ((40vh / 1.69) / 1.69)); left: calc(50vw - (40vh / 2));");
-    }
-}
-
 
 const starTransistionGeometry = new THREE.BufferGeometry();
 let isStarTransition = false;
@@ -264,6 +226,15 @@ function starFieldTransistion() {
     camera.position.z += 1000;
     metorite.position.z += 1000;
     pointLight.position.z += 1000;
+
+    setTimeout(() => {
+        camera.position.z = 0;
+        metorite.position.z = 0;
+        pointLight.position.z = 0;
+        isStarTransition = false;
+        console.log('Transitioning to phases');
+        startPhases();
+    }, 1000);
 }
 
 document.addEventListener('mousedown', (event) => {
